469d9f3d947a5bc46668259c58eea8246ca4daf8
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2022-03-28 08:54:20 -0700

    need more

--- sources/Notebook.cpp
+++ sources/Notebook.cpp
@@ -37,15 +37,14 @@ namespace ariel{
     */
     void Notebook::write(int page, int row , int col, Direction dir ,  string const &msg){
         if(page < 0 || row < 0 || col < 0 || col > 100){
-        throw std::invalid_argument("Bad Inputs - your page, row and col need to be positive and col need to be under 100"); 
+        throw invalid_argument("Bad Inputs - your page, row and col need to be positive and col need to be under 100"); 
          }
         //notebook[page].write(page, row, col, dir, msg);
-
+        int tmp;
+        tmp = msg.length();
         
         if (dir == Direction::Horizontal) {
-            int tmp;
-            tmp = msg.length();
-
+            
             //end() – Returns an iterator to the theoretical element that follows the last element in the map.
             //map find()-Returns an iterator to the element with key-value ‘g’ in the map if found, else returns the iterator to end.
             if(this->notebook.find(page) == this->notebook.end())//if find = end it means we didnt found the page
@@ -67,40 +66,54 @@ namespace ariel{
             for(int i=0; i<tmp; i++){ 
                 if(col+i>100)
                 {
-                    throw("Error - you can't write here");
+                    throw invalid_argument("Error - you can't write here");
                 }
-
-                if(this->notebook.at(page).at(row).at(col+i) != '_'|| this->notebook.at(page).at(row).at(col+i) != '~'){
-                    throw("Error - already written/erased erea");
-                }
-                else{ //we can write
-                    this->notebook.at(page).at(row).at(col+i) = msg.at(i);
+                if(this->notebook.at(page).at(row).at(col+i) != '_'|| this->notebook.at(page).at(row).at(col+i) == '~'){
+                    throw invalid_argument("Error - already written/erased erea");
                 }
             }
-
-
-
-
-
-
-
-            for (int j = 0, i = col; i < tmp; j++, i++) 
-            {
-                if (msg[j] == '~' || notebook[page][row][i] == '~') {
-                    throw invalid_argument("message error");
-                }
-                if (notebook[page][row][i] == '_') {
-                    notebook[page][row][i] = msg[j];
-                } else {
-                    throw invalid_argument("Position error");
-                } 
+            for(int i=0; i<tmp; i++){  //now - we can write
+                this->notebook.at(page).at(row).at(col+i) = msg.at(i);
             }
         }
-        else{
-            int tmp;
-            tmp = row + msg.length();
+        else{ //direction = Vertical
+            //check if page is not in the map
+            if(this->notebook.find(page) == this->notebook.end()){ 
+                //create page
+                vector <char> vechar(100, '_');
+                this->notebook[page][row] = vechar;
+            }
+            for(int i=0; i<tmp; i++){
+                //now check if row is in the page - or if we have enough
+                if(this->notebook.at(page).find(row + i) == this->notebook.at(page).end()){ 
+                vector <char> vec(100, '_');
+                this->notebook.at(page)[row + i] = vec;
+                }
+            }
+             //check if we can write
+            for(int i=0; i<tmp; i++){ 
+                if(col+i>100)
+                {
+                    throw runtime_error("Error - you can't write here");
+                }
+                if(this->notebook.at(page).at(row+i).at(col) != '_'|| this->notebook.at(page).at(row+i).at(col) == '~'){
+                    throw runtime_error("Error - already written/erased erea");
+                }
+            }
+            for(int i=0; i<tmp; i++){  //now - we can write
+                this->notebook.at(page).at(row+i).at(col) = msg.at(i);
+            }
+
+        }
+
+
+
+        
+        // else{
+        //     int tmp;
+        //     tmp = row + msg.length();
             
-        }
+        // }
 
 
 
@@ -113,33 +126,34 @@ namespace ariel{
         int tmp;
         if(page < 0 || row < 0 || col < 0 || col > 100){
         throw std::invalid_argument("Bad Inputs - your page, row and col need to be positive and col need to be under 100"); 
-         }
-         if (dir==Direction::Horizontal)
+        }
+        if (dir==Direction::Horizontal)
             {
                 tmp = col+num; //where we want to star reading.
             for (int i = col; i < tmp; i++)
                 {
-                str= str + this->notebook[page][row].at(i);
+                str += this->notebook[page][row].at(i);
                 }
             }
-        else{ //dir = Vertical
+        //dir = Vertical
+        else{ 
             tmp = row+num; //where we want to star reading.
             for (int i = row; i < tmp; i++)
                 {
-                    str= str+ this->notebook[page][i].at(col);
+                    str+= this->notebook[page][i].at(col);
                 }
         }
         return str;
-     } 
+    } 
 
 
     void Notebook::erase(int page, int row, int col, Direction dir, int num ){
         if(page < 0 || row < 0 || col < 0 || col > 100){
-            throw std::invalid_argument("Bad Inputs - your page, row and col need to be positive and col need to be under 100"); 
+            throw invalid_argument("Bad Inputs - your page, row and col need to be positive and col need to be under 100"); 
         }
         if (dir!=Direction::Horizontal || dir!=Direction::Vertical)
         {
-            throw std::invalid_argument("Bad Inputs - Direction needs to be Horizonal or Vertical"); 
+            throw invalid_argument("Bad Inputs - Direction needs to be Horizonal or Vertical"); 
         }
 
         int tmp;
@@ -162,25 +176,25 @@ namespace ariel{
 
 
     void Notebook:: show(unsigned int page){
-        if(page<0){
-        throw out_of_range("bad input - the Page need to be positive");
-    }
+        if(page<=0){
+            throw out_of_range("bad input - the Page need to be positive");
+        }
         //here we pass over the row (line)
         for (unsigned int i = 0; i <= 99; i++) 
         {
             // here we pass over the collumn
             for (unsigned int j = 0; j <= 99; j++) 
             {
-                if(notebook[page][i][j]!=0)
+                if(notebook[page][i][j]!=DEAFULT_CHAR){
                     cout << notebook[page][i][j];
+                }
                 else{
                 cout<<'_';
                 }
-                
-                cout << this->notebook[page][i][j];
             }
             cout << endl;
         }
     
     }
  }
+ 
