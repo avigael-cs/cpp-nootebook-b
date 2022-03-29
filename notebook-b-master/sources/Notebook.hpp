fdb15651cb1f65c092d3dbac39be4f67ffacb200
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2022-03-29 08:13:58 -0700

    updraded

---
+++ notebook-b-master/sources/Notebook.hpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <map>
+#include "Direction.hpp"
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <limits.h>
+#include <map>
+
+#define DEFAULT_CHAR '_'
+#define ERASE_CHAR '~'
+
+
+using namespace std;
+using namespace ariel;
+
+namespace ariel
+{
+    const int line = 100;
+    class Notebook{
+        private:
+            
+
+            /*unordered_map when 
+            *You need to keep count of some data (Example – strings) and no ordering is required.
+            *You need single element access i.e. no traversal.
+            */
+           //our book (page,row, col = char)
+            unordered_map< int, unordered_map< int, vector<char>>> notebook;
+            
+
+
+        public:
+            Notebook();
+            void arg_check(int page, int row, int column, int num, ariel::Direction dir);
+            Notebook(int page, int row, int col);
+            void write(int page, int row , int col, Direction dir ,  string const &msg);
+            std::string read(int page, int row, int col, Direction dir, int num);
+            void erase(int page, int row, int col, Direction dir, int num );
+            void show(int page);
+            
+            
+    };
+}
